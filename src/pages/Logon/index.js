60d58c86149e8cec14b0c7c081056fb85da89e73
otoniricardo <otoniricardo@gmail.com>
2020-03-29 17:49:33 -0300

    Implementa rotas

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
@@ -20,10 +21,9 @@ export default function Logon() {
             Entrar
           </button>
 
-          <a href="/register">
-            {' '}
+          <Link to="/register">
             <FiLogIn size={16} color="E02041" /> Não Tenho Cadastro
-          </a>
+          </Link>
         </form>
       </section>
       <img src={heroesImg} alt="heroes" />
