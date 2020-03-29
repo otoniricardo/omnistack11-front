208e374f0f787c5cc2e56deca163af5e7712a360
otoniricardo <otoniricardo@gmail.com>
2020-03-29 18:10:23 -0300

    Cria pagina de cadastro

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,7 +1,43 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
 import './styles.css';
 
+import logo from '../../assets/logo.svg';
+
 export default function Register() {
-  return <h1>Register</h1>;
+  return (
+    <div className="register-container">
+      <div className="content">
+        <section>
+          <img src={logo} alt="Be the Hero" />
+          <h1>Cadastro</h1>
+          <p>
+            Faça seu cadastro, entre na plataforma e ajude pessoas a encontar os
+            casos da sua ONG.
+          </p>
+
+          <Link className="back-link" to="/">
+            <FiArrowLeft size={16} color="E02041" /> Não Tenho Cadastro
+          </Link>
+        </section>
+
+        <form action="">
+          <input placeholder="Nome da ONG" />
+          <input type="email" placeholder="E-mail" />
+          <input type="text" placeholder="Whatsapp" />
+          <div className="input-group">
+            <input type="text" placeholder="Cidade" />
+            <input type="text" placeholder="UF" style={{ width: 80 }} />
+          </div>
+
+          <button className="button" type="submit">
+            Cadastrar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
